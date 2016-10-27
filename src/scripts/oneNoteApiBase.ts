7cb57fb8b2b890e2cbf128de676a5e866076a975
Vishaal Kalwani <vishaalkal@gmail.com>
2016-10-27 12:31:25 -0700

    Use the content-type module from npm to parse the content type of the responses from OneNoteApi

--- src/scripts/oneNoteApiBase.ts
+++ src/scripts/oneNoteApiBase.ts
@@ -1,6 +1,6 @@
 /// <reference path="../definitions/es6-promise/es6-promise.d.ts"/>
 /// <reference path="../oneNoteApi.d.ts"/>
-/// <reference path="../node_modules/@types/content-type/index.d.ts">
+/// <reference path="../definitions/content-type/content-type.d.ts"/>
 
 import {ErrorUtils, RequestErrorType} from "./errorUtils";
 
