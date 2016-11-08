0b1e8c4d18d214ef9dbf81a3fadddbf40002e612
Matthew Chiam <machiam@microsoft.com>
2016-11-08 11:17:52 -0800

    Cleanup

---
+++ src/scripts/iOneNoteApi.ts
@@ -0,0 +1,17 @@
+import {ResponsePackage} from "./oneNoteApiBase";
+import {OneNotePage} from "./oneNotePage";
+import {Revision} from "./structuredTypes";
+
+export interface IOneNoteApi {
+	createNotebook(name: string): Promise<ResponsePackage<any>>;
+	createPage(page: OneNotePage, sectionId?: string): Promise<ResponsePackage<any>>;
+	getPage(pageId: string): Promise<ResponsePackage<any>>;
+	getPageContent(pageId: string): Promise<ResponsePackage<any>>;
+	getPages(options: { top?: number, sectionId?: string }): Promise<ResponsePackage<any>>;
+	updatePage(pageId: string, revisions: Revision[]): Promise<ResponsePackage<any>>;
+	createSection(notebookId: string, name: string): Promise<ResponsePackage<any>>;
+	getNotebooks(excludeReadOnlyNotebooks?: boolean): Promise<ResponsePackage<any>>;
+	getNotebooksWithExpandedSections(expands?: number, excludeReadOnlyNotebooks?: boolean): Promise<ResponsePackage<any>>;
+	getNotebookByName(name: string): Promise<ResponsePackage<any>>;
+	pagesSearch(query: string): Promise<ResponsePackage<any>>;
+}
